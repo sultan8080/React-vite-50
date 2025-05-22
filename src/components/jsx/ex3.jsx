c2a56b6e272f6ad5f990ed5864cd521231b939eb
Moti-Sultan <motisultannur@gmail.com>
2025-05-22 09:27:59 +0200

    Ex-3: Liste d’éléments JSX (ex: fruits)

--- src/components/jsx/ex3.jsx
+++ src/components/jsx/ex3.jsx
@@ -1,1 +1,10 @@
 // Exo-3. Afficher une liste de 3 éléments (comme tes fruits préférés).
+export default function Ex3() {
+  return (
+    <ul style={{ listStyleType: 'circle', fontSize: '18px', color: 'green' }}>
+      <li>Pomme</li>
+      <li>Banane</li>
+      <li>Fraise</li>
+    </ul>
+  );
+}
